53078c4e300c80d7a3793e7443445663b8c82792
Sergei Maksimov.com <s.mksimov@gmail.com>
2021-08-27 23:03:09 +0300

    refactored Testing

--- src/component/Testing.tsx
+++ src/component/Testing.tsx
@@ -1,37 +1,24 @@
 import React from 'react';
-import { State, TestQuestionsType, TestResultsType } from '../types/types';
+import { TestQuestionsType } from '../types/types';
 import { QuestionsItem } from './QuestionsItem';
 
 type IntroductionType = {
-  state: State
-  finishTesting: () => void
   testQuestions: TestQuestionsType
-  saveResult: (result: TestResultsType) => void
-  saveCurrentQuestion: (currentQuestion: number) => void
 };
 
 export const Testing: React.FC<IntroductionType> = ({
-                                                      state,
-                                                      finishTesting,
                                                       testQuestions,
-                                                      saveResult,
-                                                      saveCurrentQuestion,
                                                     }) => {
-
 
   const questionsQuantity = testQuestions.length;
 
   return <div className='mainBlock'>
     {
       testQuestions.map((q, index) => <QuestionsItem
-        state={state}
         key={q.id}
         question={q}
         quantity={questionsQuantity}
         index={index}
-        finishTesting={finishTesting}
-        saveResult={saveResult}
-        saveCurrentQuestion={saveCurrentQuestion}
       />)
     }
   </div>
